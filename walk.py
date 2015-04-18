7f85cab099aff523d0ab832018726894da977424
bucho <steelwind666@gmail.com>
2015-04-18 15:26:38 +0900

    色と日本語に対応 modified:   walk.py

--- walk.py
+++ walk.py
@@ -9,28 +9,38 @@ class ScreenBuffer(object):
     self._updated = set()
     self.fill('&nbsp')
 
-  def fill(self, g):
+  def fill(self, g, color='silver'):
     (w, h) = self._size
     for y in range(h):
       for x in range(w):
-        if self._grid[y][x] == g: continue
-        self._grid[y][x] = g
-        self._updated.add(y)
+        self.put(g, (x, y), color)
 
   def flush(self, socket):
-    data = ['%d:%s' % (y, "".join(self._grid[y])) for y in self._updated]
+    data = []
+    current_color = None
+    for y in self._updated:
+      line = '%d:<font color="silver">' % y
+      for ch, color in self._grid[y]:
+        if color == current_color:
+          line += ch
+        else:
+          line += '</font><font color=%s>%s' % (color, ch)
+          current_color = color
+      line += '</font>'
+      data.append(line)
     data.append('flip')
     socket.send('\x00'.join(data))
     self._updated.clear()
 
-  def put(self, g, (x, y)):
-    if self._grid[y][x] == g: return
-    self._grid[y][x] = g
+  def put(self, g, (x, y), color='silver'):
+    g = g.encode('utf-8')
+    if self._grid[y][x] == (g, color): return
+    self._grid[y][x] = (g, color)
     self._updated.add(y)
 
-  def write(self, string, (x, y)):
+  def write(self, string, (x, y), color):
     for i, ch in enumerate(string):
-      self.put(ch, (x+i, y));
+      self.put(ch, (x+i, y), color);
 
 class Service(object):
   def __init__(self):
@@ -39,6 +49,7 @@ class Service(object):
 
   def enter(self, socket):
     self._screen = ScreenBuffer((80, 21))
+    self._screen.flush(socket)
 
   def leave(self, socket):
     pass
@@ -50,8 +61,8 @@ class Service(object):
     if key == 'h': self._x -= 1
     if key == 'j': self._y += 1
     if key == 'k': self._y -= 1
-    self._screen.put('@', (self._x, self._y))
-    self._screen.write('%s' % key, (0, 15))
+    self._screen.put('@', (self._x, self._y), 'olive')
+    self._screen.write(u'入力キー:%s' % key, (4, 20), 'lime')
     self._screen.flush(socket)
 
 if __name__ == '__main__':
